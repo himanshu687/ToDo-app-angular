1f9539177b3d434b0129ea7b17e19b0801982c91
Himanshu <himanshuhisar687@gmail.com>
2023-09-29 11:20:05 +0530

    pwa added with cache property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -22,6 +22,7 @@ import { TodosService } from './pages/todos/todos.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LoaderComponent } from './components/loader/loader.component';
 import { TodoDetailsModalComponent } from './pages/todos/todo-details-modal/todo-details-modal.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -47,6 +48,12 @@ import { TodoDetailsModalComponent } from './pages/todos/todo-details-modal/todo
     MatProgressSpinnerModule,
     MatPaginatorModule,
     MatDialogModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [TodosService],
   bootstrap: [AppComponent],
